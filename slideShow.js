e16f0294eef062bb1f594eb0d55e3017860f1e9a
MaximCamilleri <max.camilleri.20@um.edu.mt>
2021-03-25 14:09:10 +0100

    fixed slideshow

--- slideShow.js
+++ slideShow.js
@@ -1,4 +1,5 @@
 var slideIndex = 1;
+var slides = document.getElementsByClassName("mySlides");
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -13,7 +14,6 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
