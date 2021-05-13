1b3e31f37556c50c37a6e9bd7a04963cef12ae5b
Voben <mathias.vaaben.20@um.edu.mt>
2021-05-13 10:30:49 +0200

    Removed some files and added menuStyles

---
+++ menu.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+    <head>
+
+        <meta charset="UTF-8">
+        <title>PogFish Sushi</title>
+        <!-- <link rel="stylesheet" href="contactStyles.css"> -->
+        <link rel="stylesheet" href="loginCSS.css">
+
+        <!--Fonts-->
+        <link rel="preconnect" href="https://fonts.gstatic.com/%22%3E">
+        <link href="https://fonts.googleapis.com/css2?family=Philosopher:wght@700&family=Raleway:wght@300&family=Ubuntu&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+
+    
+
+    </body>
+</html>
