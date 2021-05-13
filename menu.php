ef787b5802d6f2182cd1aef5a749f2ade709f94a
Voben <mathias.vaaben.20@um.edu.mt>
2021-05-13 11:34:00 +0200

    started on separate css files, found placeholder images and created some more php files

--- menu.php
+++ menu.php
@@ -6,6 +6,12 @@
         <title>PogFish Sushi</title>
         <!-- <link rel="stylesheet" href="contactStyles.css"> -->
         <link rel="stylesheet" href="loginCSS.css">
+        <!--<link rel="stylesheet" href="styles.css">-->
+        <link rel="stylesheet" href="navbarStyles.css">
+
+        <!-- Gallery links -->
+        <link href="gallery.css" rel="stylesheet">
+        <script src="gallery.js"></script>
 
         <!--Fonts-->
         <link rel="preconnect" href="https://fonts.gstatic.com/%22%3E">
@@ -14,7 +20,22 @@
     </head>
     <body>
 
-    
+    <?php
+     include 'header2.php';        
+    ?>
+
+    <main>
+
+    <div class="gallery">
+
+    <?php
+    $images = glob("images/gallery/". "*.{jpg,jpeg,gif,png,bmp,webp}");
+    foreach ($images as $i) {
+        printf("<img src='gallery/%s'/>", basename($i));
+      }
+    ?>
+
+    </main>
 
     </body>
 </html>
